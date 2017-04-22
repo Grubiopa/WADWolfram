0280310a08671f9245c5e2dde74d1de0f6ee9d86
KarenPAPP <k.principe@alumnos.urjc.es>
2017-04-22 21:32:31 +0200

    Bean for Angular

--- WADWolfram/src/main/java/com/mycompany/mavenproject1/Application.java
+++ WADWolfram/src/main/java/com/mycompany/mavenproject1/Application.java
@@ -18,21 +18,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @SpringBootApplication
 public class Application {
-
-    @Bean
-    public WebMvcConfigurerAdapter corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
-
     
     public static void main(String[] args){
         
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+
+                .allowCredentials(true).maxAge(3600);
+
+            }
+
+        };
+
     }
     
 }

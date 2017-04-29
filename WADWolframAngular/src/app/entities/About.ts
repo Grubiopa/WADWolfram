88bf34076bf9fe8983cb1e922a7cf14ed0fd3a0e
JuanAntonioPares <ja.pares@alumnos.urjc.es>
2017-04-29 10:33:40 +0200

    breadcrums and styles

--- WADWolframAngular/src/app/entities/About.ts
+++ WADWolframAngular/src/app/entities/About.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about',
   templateUrl: './about.html',
-  styleUrls: ['../app.component.css','../../assets/bodybg/bg1.css']
+  styleUrls: ['../app.component.css', 'about.css' ]
+  
 })
 
 export class AboutComponent{}

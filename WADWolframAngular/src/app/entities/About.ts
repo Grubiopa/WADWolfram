2c25739e20c32e8a5f74637e6d01266923db53d8
JuanAntonioPares <ja.pares@alumnos.urjc.es>
2017-04-29 14:04:47 +0200

    final css styles added
    about and contact

--- WADWolframAngular/src/app/entities/About.ts
+++ WADWolframAngular/src/app/entities/About.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about',
   templateUrl: './about.html',
-  styleUrls: ['../app.component.css', 'about.css' ]
+  styleUrls: ['about.css','../app.component.css', '../../assets/css/bootstrap.min.css', '../../assets/css/animate.css', '../../assets/css/cubeportfolio.min.css'
+  , '../../assets/css/custom-fonts.css', '../../assets/css/flexslider.css', '../../assets/css/font-awesome.css', '../../assets/css/overwrite.css'
+  , '../../assets/css/style.css', '../../assets/css/theme.css', '../../assets/css/user.css']
   
 })
 

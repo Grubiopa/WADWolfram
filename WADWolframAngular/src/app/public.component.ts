aac97e5505d809e164cfd62f0247e8b4c4959f72
JuanAntonioPares <ja.pares@alumnos.urjc.es>
2017-05-04 21:14:44 +0200

    dropdown and links (fonts) errors fixed

--- WADWolframAngular/src/app/public.component.ts
+++ WADWolframAngular/src/app/public.component.ts
@@ -3,6 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   	selector: 'public',
     templateUrl: './public.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css', '../assets/css/bootstrap.min.css', '../assets/css/animate.css', '../assets/css/cubeportfolio.min.css'
+  , '../assets/css/custom-fonts.css', '../assets/css/flexslider.css', '../assets/css/font-awesome.css', '../assets/css/overwrite.css'
+  , '../assets/css/style.css', '../assets/css/theme.css', '../assets/css/user.css']
 })
-export class PublicComponent {}
+export class PublicComponent {
+  
+  private menuCollapse: boolean = true;
+  private showMenu: boolean = true;
+  
+  showDropdown(typeMenu: string) {// Método encargado de los dropdowns del navbar
+
+    if (typeMenu === "collapse") {
+
+      this.menuCollapse = !this.menuCollapse;
+
+    } else if (typeMenu === "loginMenu") {
+
+      this.showMenu = !this.showMenu;
+
+    }
+
+  }
+
+}

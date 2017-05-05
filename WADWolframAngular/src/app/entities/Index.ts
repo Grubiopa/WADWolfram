07f83a041d92ebebf43de23d726c94cddbea2493
stimonm <sandra2788@gmail.com>
2017-05-05 22:38:07 +0200

    html and ts index have been changed in order to show projects and news lists

--- WADWolframAngular/src/app/entities/Index.ts
+++ WADWolframAngular/src/app/entities/Index.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../Class/Project';
+import { ProjectService } from '../services/ProjectService';
+import { New } from '../Class/New';
+import { NewService } from '../services/NewService';
+import { ProjectProgress } from '../Class/ProjectProgress';
 
 @Component({
   selector: 'index',
@@ -6,4 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['../app.component.css']
 })
 
-export class IndexComponent{}
+export class IndexComponent implements OnInit {
+  projects: Project[] = [];
+  news: New[] = [];
+  projectsProgress: ProjectProgress[] = [];
+
+   constructor(private projectService: ProjectService, private newService: NewService){}
+   ngOnInit(){
+      this.projectService.getProjects().subscribe(
+         projects => this.projects = projects,
+         error => console.log(error)
+      );
+      this.newService.getNews().subscribe(
+         news => this.news = news,
+         error => console.log(error)
+      );
+      for(let project of this.projects){
+         let p: ProjectProgress = new ProjectProgress(project.title, (project.parcialBudget*100/project.totalBudget));
+         this.projectsProgress.push(p);         
+      }
+   }
+}

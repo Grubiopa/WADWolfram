d59b4b204fb59b7820c2a5c37c5708363994443e
KarenPAPP <k.principe@alumnos.urjc.es>
2017-05-06 01:57:58 +0200

    Revert "html and ts index have been changed in order to show projects and news lists"
    This reverts commit 07f83a041d92ebebf43de23d726c94cddbea2493.

--- WADWolframAngular/src/app/entities/Index.ts
+++ WADWolframAngular/src/app/entities/Index.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Project } from '../Class/Project';
-import { ProjectService } from '../services/ProjectService';
-import { New } from '../Class/New';
-import { NewService } from '../services/NewService';
-import { ProjectProgress } from '../Class/ProjectProgress';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'index',
@@ -11,24 +6,4 @@ import { ProjectProgress } from '../Class/ProjectProgress';
   styleUrls: ['../app.component.css']
 })
 
-export class IndexComponent implements OnInit {
-  projects: Project[] = [];
-  news: New[] = [];
-  projectsProgress: ProjectProgress[] = [];
-
-   constructor(private projectService: ProjectService, private newService: NewService){}
-   ngOnInit(){
-      this.projectService.getProjects().subscribe(
-         projects => this.projects = projects,
-         error => console.log(error)
-      );
-      this.newService.getNews().subscribe(
-         news => this.news = news,
-         error => console.log(error)
-      );
-      for(let project of this.projects){
-         let p: ProjectProgress = new ProjectProgress(project.title, (project.parcialBudget*100/project.totalBudget));
-         this.projectsProgress.push(p);         
-      }
-   }
-}
+export class IndexComponent{}
